25f3b348800d7cc668c7fd4f42953336e62d1d5b
Rojen-Maharjan <er.rojenmaharjan@gmail.com>
2020-11-24 19:29:56 +0545

    GOG-23 remove backend from repo

--- backend/api/views.py
+++
@@ -1,9 +0,0 @@
-from api.models import Food
-from django.core import serializers
-from django.http import JsonResponse
-from utils.response_format_helper import successApi
-
-def index(request):
-    data = Food.objects.all()
-    serialized = serializers.serialize('json',data)
-    return successApi('success',serialized)
--- backend/readme.md
+++
@@ -1,9 +0,0 @@
-### Backend for api store and fetch
-
-```
-py -3 -m venv venv
-pip install -r requirements.txt 
-
-venv\Scripts\activate
-(venv) python manage.py runserver
-```
