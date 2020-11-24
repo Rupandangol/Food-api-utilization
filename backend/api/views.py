4713d0cad9cc7406bcf17cbf5e1aa44e5f9aeb67
Rojen-Maharjan <er.rojenmaharjan@gmail.com>
2020-11-24 18:43:42 +0545

    GOG-27 update error http response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,9 @@
 from api.models import Food
 from django.core import serializers
-from django.http import JsonResponse
-from utils.response_format_helper import successApi
+from utils.response_format_helper import successApi, errorApi
+
 
 def index(request):
     data = Food.objects.all()
-    serialized = serializers.serialize('json',data)
-    return JsonResponse(successApi('success',serialized))
+    serialized = serializers.serialize('json', data)
+    return successApi('validation', serialized)
--- backend/utils/response_format_helper.py
+++ backend/utils/response_format_helper.py
@@ -1,32 +1,33 @@
+import json
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponseBadRequest
+
+
 def errorApi(type, data):
     if (type == "not_found"):
-        return {
-            "state": "error",
-            "code": 404,
-            "message": "Not found",
-            "data": data
-        }
+        return HttpResponseNotFound('Not found')
+
     if (type == "validation"):
-        return {
-            "state": "error",
-            "code": 422,
-            "message": "validation failed",
-            "data": data
-        }
-    return {"state": "error", "code": 400, "message": "Error", "data": data}
+        return HttpResponseBadRequest(
+            json.dumps({
+                "state": "error",
+                "code": 422,
+                "message": "validation failed",
+                "data": data
+            }))
+    return HttpResponseNotFound('Not found')
 
 
 def successApi(type, data):
     if (type == "success"):
-        return {
+        return JsonResponse({
             "state": "success",
             "code": 200,
             "message": "Success",
             "data": data
-        }
-    return {
+        })
+    return JsonResponse({
         "state": "success",
         "code": 200,
         "message": "success",
         "data": data
-    }
+    })
